ff381f193d3464888179dff77ead945b3cbd32b5
marianalopesz <mariana.lopes@sptech.school>
2024-05-25 19:45:52 -0300

    adicionando validações de cadastro e login

---
+++ js/cadastro.js
@@ -0,0 +1,26 @@
+function cadastrar() {
+    const email = input_email.value;
+    const senha = input_senha.value;
+    const confirmarSenha = input_confirmar_senha.value;
+    const nome = input_nome.value;
+    if (email != "" || senha != "" || confirmarSenha != "" || nome != "") {
+        for (let posicao = 0; posicao < email.length; posicao++) {
+            if (email[posicao] == '@') {
+                break;
+            }else{
+                alert('Preencha todos os campos corretamente!')
+            }
+        }
+        if (senha.length <= 10) {
+            if (confirmarSenha == senha) {
+                window.location = "../Login e Cadastro/login.html";
+            } else {
+                alert('Preencha todos os campos corretamente!');
+            }
+        } else {
+            alert('Preencha todos os campos corretamente!');
+        }
+    } else {
+        alert('Preencha todos os campos corretamente!');
+    }
+}
---
+++ js/login.js
@@ -0,0 +1,7 @@
+function direcionarDash() {
+    const email = input_email.value;
+    const senha = input_senha.value;
+    if (email == "" || senha == "") {
+        alert('Preencha todos os campos!');
+    }
+}
