51419d1c4d9cff7eb1e71cfdcfadff805656aadf
marianalopesz <mariana.lopes@sptech.school>
2024-05-26 03:06:09 -0300

    atualizando telas da dashboard

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,8 +1,9 @@
 function cadastrar() {
+    const nome = input_nome.value;
     const email = input_email.value;
     const senha = input_senha.value;
     const confirmarSenha = input_confirmar_senha.value;
-    const nome = input_nome.value;
+
     if (email != "" || senha != "" || confirmarSenha != "" || nome != "") {
         for (let posicao = 0; posicao < email.length; posicao++) {
             if (email[posicao] == '@') {
@@ -23,4 +24,44 @@ function cadastrar() {
     } else {
         alert('Preencha todos os campos corretamente!');
     }
+
+    fetch("/usuarios/cadastrar", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          // crie um atributo que recebe o valor recuperado aqui
+          // Agora vá para o arquivo routes/usuario.js
+          nomeServer: nome,
+          emailServer: email,
+          senhaServer: senha
+        }),
+      })
+        .then(function (resposta) {
+          console.log("resposta: ", resposta);
+  
+          if (resposta.ok) {
+  
+            alert("Cadastro realizado com sucesso! Redirecionando para tela de Login...")
+  
+            setTimeout(() => {
+              window.location = "login.html";
+            }, "2000");
+  
+            limparFormulario();
+          } else {
+            throw alert("Houve um erro ao tentar realizar o cadastro!");
+          }
+        })
+        .catch(function (resposta) {
+          console.log(`#ERRO: ${resposta}`);
+        });
+  
+      return false;
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location = "../Login e Cadastro/login.html";
 }
--- js/login.js
+++ js/login.js
@@ -3,5 +3,17 @@ function direcionarDash() {
     const senha = input_senha.value;
     if (email == "" || senha == "") {
         alert('Preencha todos os campos!');
+    }else{
+        window.location = "../Dashboard/dashboard.html";
     }
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: email,
+            senhaServer: senha
+        })
+    })
 }
