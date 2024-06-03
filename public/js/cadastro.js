dda08165c841a6762929b1f349c31611e3cea546
marianalopesz <mariana.lopes@sptech.school>
2024-06-03 16:19:04 -0300

    api conectada com o cadastro e login, perguntas da cruzadinha atualizada

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -6,11 +6,11 @@ function cadastrar() {
 
     if (email != "" || senha != "" || confirmarSenha != "" || nome != "") {
         for (let posicao = 0; posicao < email.length; posicao++) {
-            if (email[posicao] == '@') {
+           /* if (email[posicao] == '@') {
                 break;
             }else{
                 alert('Preencha todos os campos corretamente!')
-            }
+            }*/
         }
         if (senha.length <= 10) {
             if (confirmarSenha == senha) {
@@ -24,44 +24,4 @@ function cadastrar() {
     } else {
         alert('Preencha todos os campos corretamente!');
     }
-
-    fetch("/usuarios/cadastrar", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          // crie um atributo que recebe o valor recuperado aqui
-          // Agora vá para o arquivo routes/usuario.js
-          nomeServer: nome,
-          emailServer: email,
-          senhaServer: senha
-        }),
-      })
-        .then(function (resposta) {
-          console.log("resposta: ", resposta);
-  
-          if (resposta.ok) {
-  
-            alert("Cadastro realizado com sucesso! Redirecionando para tela de Login...")
-  
-            setTimeout(() => {
-              window.location = "login.html";
-            }, "2000");
-  
-            limparFormulario();
-          } else {
-            throw alert("Houve um erro ao tentar realizar o cadastro!");
-          }
-        })
-        .catch(function (resposta) {
-          console.log(`#ERRO: ${resposta}`);
-        });
-  
-      return false;
-}
-
-function limparSessao() {
-    sessionStorage.clear();
-    window.location = "../Login e Cadastro/login.html";
-}
+  }
